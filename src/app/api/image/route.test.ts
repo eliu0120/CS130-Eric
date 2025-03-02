b7435c1d528d645e1a7e132f9cf690a9f3c7e71d
KevZ1209 <kevinbowditch11@gmail.com>
2025-03-01 18:00:02 -0800

    add uuid prefix to image uploads. update unit tests

--- src/app/api/image/route.test.ts
+++ src/app/api/image/route.test.ts
@@ -1,23 +1,84 @@
-// __tests__/image.test.ts
-import { POST } from "./route"; // Adjust the path as needed
-import { NextResponse } from "next/server";
+import { POST } from "./route";
+import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid"; // Import uuidv4
+
+const { ref, uploadBytes, getDownloadURL } =
+  jest.requireMock("firebase/storage");
+
+jest.mock("@/lib/firebase/config", () => ({
+  storage: "STORAGE",
+}));
+
+jest.mock("firebase/storage", () => {
+  const originalModule = jest.requireActual("firebase/storage");
+  return {
+    ...originalModule,
+    ref: jest.fn((storage, path) => path),
+    uploadBytes: jest.fn(),
+    getDownloadURL: jest.fn((ref) => ref),
+  };
+});
+
+jest.mock("uuid", () => ({
+  v4: jest.fn(() => "mocked-uuid"), // Mock uuidv4
+}));
 
 describe("/api/image", () => {
-  it("should return an error if no image is provided", async () => {
-    // Create a mock request object
-    const mockRequest = {
-      formData: () => Promise.resolve(new FormData()), // Mock formData
-    } as any;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (uuidv4 as jest.Mock).mockClear(); // Clear uuid mock
+  });
 
-    // Call the API route's POST function
+  it("should return an error if no image is provided", async () => {
+    const mockRequest = {
+      formData: () => Promise.resolve(new FormData()),
+    } as unknown as NextRequest;
+
     const response = await POST(mockRequest);
 
-    // Check response to be NextResponse
     expect(response).toBeInstanceOf(NextResponse);
 
-    // to check json fields
     const { data, error } = await response.json();
     expect(data).toBe(null);
     expect(error).toBe("No image provided");
+  });
+
+  it("should return url to uploaded image with uuid prefix", async () => {
+    const mockRequest = {
+      formData: () =>
+        Promise.resolve({
+          get: () => {
+            return {
+              name: "test_image.png",
+              arrayBuffer: () => {
+                return "test_image_buffer.png";
+              },
+            };
+          },
+        }),
+    } as unknown as NextRequest;
+
+    const response = await POST(mockRequest);
+
+    expect(response).toBeInstanceOf(NextResponse);
+
+    const { data, error } = await response.json();
+    expect(data).toBe("images/mocked-uuid-test_image.png");
+    expect(error).toBe(null);
+
+    expect(uuidv4).toHaveBeenCalled(); // Check uuidv4 was called
+
+    expect(ref.mock.calls[0]).toEqual([
+      "STORAGE",
+      "images/mocked-uuid-test_image.png",
+    ]);
+    const exp_buffer = Buffer.from("test_image_buffer.png");
+    expect(uploadBytes.mock.calls[0]).toEqual([
+      "images/mocked-uuid-test_image.png",
+      exp_buffer,
+    ]);
+    expect(getDownloadURL.mock.calls[0]).toEqual([
+      "images/mocked-uuid-test_image.png",
+    ]);
   });
 });
--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -2,6 +2,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage } from "@/lib/firebase/config";
+import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: NextRequest) {
   try {
@@ -15,7 +16,10 @@ export async function POST(request: NextRequest) {
     const bytes = await image.arrayBuffer();
     const buffer = Buffer.from(bytes);
 
-    const storageRef = ref(storage, `images/${image.name}`);
+    // Generate a unique file name
+    const uniqueFileName = `${uuidv4()}-${image.name}`;
+
+    const storageRef = ref(storage, `images/${uniqueFileName}`);
     await uploadBytes(storageRef, buffer); // Use the Buffer
     const url = await getDownloadURL(storageRef);
 
